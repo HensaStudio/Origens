ecff9156e3ac1fdaf91d597f58ff6c5c95b53505
Lucas Hen <soulucashen@icloud.com>
2026-02-24 02:18:48 -0300

    Update fxmanifest.lua

--- Server/resources/[maps]/cfx-hensa/fxmanifest.lua
+++ Server/resources/[maps]/cfx-hensa/fxmanifest.lua
@@ -6,9 +6,14 @@ this_is_a_map "yes"
 files {
 	"stream/*",
 	"stream/**/*",
+	"rel/laundromat_doors_game.dat151.rel",
+    "rel/lev_laundromat.dat151.rel",
 	"interiorproxies.meta"
 }
 
 data_file "DLC_ITYP_REQUEST" "stream/*.ytyp"
 data_file "DLC_ITYP_REQUEST" "stream/**/*.ytyp"
+
 data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+data_file "DLC_ITYP_REQUEST" "stream/Flowers/sideboard_vf.ytyp"
