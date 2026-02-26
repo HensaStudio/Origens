a3810838dc455d88ef6713a8586852771d546575
Lucas Hen <soulucashen@icloud.com>
2026-02-26 16:26:56 -0300

    Update drops.lua

--- Server/resources/[scripts]/inventory/server-side/drops.lua
+++ Server/resources/[scripts]/inventory/server-side/drops.lua
@@ -99,7 +99,7 @@ exports("Drops",function(Passport,source,Item,Amount,Force,Coords)
 		local Item = Split[1]
 
 		if not Provisory.desc then
-			if Item == "vehiclekey" and Split[3] then
+			if Item == "vehiclekey" and Split[2] then
 				local Consult = exports.oxmysql:single_async("SELECT * FROM vehicles WHERE Plate = ? LIMIT 1",{ Split[3] })
 				if Consult and VehicleExist(Consult.Vehicle) then
 					Provisory.desc = "Proprietário: <common>"..vRP.FullName(Consult.Passport).."</common><br>Modelo: <common>"..VehicleName(Consult.Vehicle).."</common><br>Placa: <common>"..Split[3].."</common>"
