890584765226160d1f9745ad696320501f9e6226
Lucas Hen <soulucashen@icloud.com>
2026-03-03 14:03:05 -0300

    Update drops.lua

--- Server/resources/[scripts]/inventory/server-side/drops.lua
+++ Server/resources/[scripts]/inventory/server-side/drops.lua
@@ -100,11 +100,11 @@ exports("Drops",function(Passport,source,Item,Amount,Force,Coords)
 
 		if not Provisory.desc then
 			if Item == "vehiclekey" and Split[2] then
-				local Consult = exports.oxmysql:single_async("SELECT * FROM vehicles WHERE Plate = ? LIMIT 1",{ Split[3] })
+				local Consult = exports.oxmysql:single_async("SELECT * FROM vehicles WHERE Plate = ? LIMIT 1",{ Split[2] })
 				if Consult and VehicleExist(Consult.Vehicle) then
-					Provisory.desc = "Proprietário: <common>"..vRP.FullName(Consult.Passport).."</common><br>Modelo: <common>"..VehicleName(Consult.Vehicle).."</common><br>Placa: <common>"..Split[3].."</common>"
+					Provisory.desc = "Proprietário: <common>"..vRP.FullName(Consult.Passport).."</common><br>Modelo: <common>"..VehicleName(Consult.Vehicle).."</common><br>Placa: <common>"..Split[2].."</common>"
 				else
-					Provisory.desc = "Proprietário: <epic>Prefeitura</epic><br>Placa: <common>"..Split[3].."</common>"
+					Provisory.desc = "Proprietário: <epic>Prefeitura</epic><br>Placa: <common>"..Split[2].."</common>"
 				end
 			elseif Item == "propertys" and Split[2] then
 				local Consult = exports.oxmysql:single_async("SELECT * FROM propertys WHERE Serial = ? LIMIT 1",{ Split[2] })
