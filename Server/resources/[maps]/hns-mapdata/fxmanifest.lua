08174fdace339d54b3a217b58da7f6755f49ac85
Hensa <soulucashen@icloud.com>
2026-05-02 20:51:37 -0300

    hns-mapdata

---
+++ Server/resources/[maps]/hns-mapdata/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+files {
+	'popgroups.ymt',
+	'popcycle.dat'
+}
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
+data_file 'POPSCHED_FILE' 'popcycle.dat'
