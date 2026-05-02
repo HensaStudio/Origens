a52b0969f828769df706eeee02fb1addebbc4d54
Hensa <soulucashen@icloud.com>
2026-05-02 20:52:15 -0300

    hns-vehicles-base

---
+++ Server/resources/[vehicles]/hns-vehicles-base/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version "adamant"
+games { "gta5" }
+
+lua54 "yes"
+
+files {
+	"stream/meta_files/clube/carcols.meta",
+	"stream/meta_files/clube/carvariations.meta",
+	"stream/meta_files/clube/handling.meta",
+	"stream/meta_files/clube/vehiclelayouts_clube.meta",
+	"stream/meta_files/clube/vehicles.meta",
+
+	"stream/meta_files/mosquito/carvariations.meta",
+	"stream/meta_files/mosquito/handling.meta",
+	"stream/meta_files/mosquito/vehicles.meta",
+
+	"stream/meta_files/piccolo/carvariations.meta",
+	"stream/meta_files/piccolo/handling.meta",
+	"stream/meta_files/piccolo/vehicles.meta",
+
+	"stream/meta_files/sgzoomer/carvariations.meta",
+	"stream/meta_files/sgzoomer/handling.meta",
+	"stream/meta_files/sgzoomer/vehicles.meta"
+}
+
+data_file "CARCOLS_FILE" "stream/meta_files/clube/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "stream/meta_files/clube/carvariations.meta"
+data_file "HANDLING_FILE" "stream/meta_files/clube/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "stream/meta_files/clube/vehiclelayouts_clube.meta"
+data_file "VEHICLE_METADATA_FILE" "stream/meta_files/clube/vehicles.meta"
+
+data_file "VEHICLE_VARIATION_FILE" "stream/meta_files/mosquito/carvariations.meta"
+data_file "HANDLING_FILE" "stream/meta_files/mosquito/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "stream/meta_files/mosquito/vehicles.meta"
+
+data_file "VEHICLE_VARIATION_FILE" "stream/meta_files/piccolo/carvariations.meta"
+data_file "HANDLING_FILE" "stream/meta_files/piccolo/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "stream/meta_files/piccolo/vehicles.meta"
+
+data_file "VEHICLE_VARIATION_FILE" "stream/meta_files/sgzoomer/carvariations.meta"
+data_file "HANDLING_FILE" "stream/meta_files/sgzoomer/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "stream/meta_files/sgzoomer/vehicles.meta"
