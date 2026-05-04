5182479748090ed79ddb09b8942dd51533c4db39
Hensa <soulucashen@icloud.com>
2026-05-04 13:15:22 -0300

    Update shared.lua

--- Server/resources/[scripts]/waypoints/shared-side/shared.lua
+++ Server/resources/[scripts]/waypoints/shared-side/shared.lua
@@ -12,15 +12,15 @@ Config.FadeStartDistance = 25
 --[[
   Exports (client-side)
 
-  local id = exports['bfx-waypoints']:AddWaypoint(pos, {
+  local id = exports['waypoints']:AddWaypoint(pos, {
     label      = "DELIVERY",       -- default "WAYPOINT"
     color      = "#E74C3C",        -- default "#FFFFFF"
     autoRemove = true,             -- default true
     removeDist = 5.0,              -- default 5.0
   })
 
-  exports['bfx-waypoints']:RemoveWaypoint(id)  --> bool
-  exports['bfx-waypoints']:UpdateWaypoint(id, { pos = vec3, label = "X", color = "#FF0000" })  --> bool
-  exports['bfx-waypoints']:GetWaypointForCoord(pos, radius?)  --> id | nil   (radius defaults to 1.0)
-  exports['bfx-waypoints']:ClearWaypoints()
+  exports['waypoints']:RemoveWaypoint(id)  --> bool
+  exports['waypoints']:UpdateWaypoint(id, { pos = vec3, label = "X", color = "#FF0000" })  --> bool
+  exports['waypoints']:GetWaypointForCoord(pos, radius?)  --> id | nil   (radius defaults to 1.0)
+  exports['waypoints']:ClearWaypoints()
 ]]
